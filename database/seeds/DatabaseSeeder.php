37e1150b907eba63b23e3165f69d7d0e298c9e21
luisferdk <luisdk.03@gmail.com>
2020-04-25 08:15:48 -0400

    Migrations and Seeders with 20 registers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +12,37 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        // $this->call(UserSeeder::class);
+        /* Users */
+        DB::table('users')->truncate();
+        for ($i=0; $i < 20; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10)
+            ]);
+        }
+
+        /* Posts */
+        DB::table('posts')->truncate();
+        for ($i=0; $i < 20; $i++) {
+            DB::table('posts')->insert([
+                'description' => $faker->paragraph(),
+                'user_id'=>$faker->numberBetween(1,20)
+            ]);
+        }
+
+        /* Comments */
+        DB::table('comments')->truncate();
+        for ($i=0; $i < 20; $i++) {
+            DB::table('comments')->insert([
+                'description' => $faker->sentence(20,true),
+                'user_id'=>$faker->numberBetween(1,20),
+                'post_id'=>$faker->numberBetween(1,20)
+            ]);
+        }
     }
 }
